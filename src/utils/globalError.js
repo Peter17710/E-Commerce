92cb837bed5909c324911b3b7b36e70c5a5bd1d4
Peter17710 <peteratef177@gmail.com>
2025-04-12 22:44:06 +0200

    Your commit message here

--- src/utils/globalError.js
+++ src/utils/globalError.js
@@ -1,12 +1,6 @@
 
-
-    // export const globalError = (err,req,res,next)=>{
-    //     res.status(err.statusCode).send(err.message);
-    // }
-
-    // filepath: c:\Users\Peter\Desktop\E-Commerce\src\utils\globalError.js
 export const globalError = (err, req, res, next) => {
-    const statusCode = err.statusCode || 500; // Default to 500 if undefined
+    const statusCode = err.statusCode || 500;
     const message = err.message || "Internal Server Error";
 
     res.status(statusCode).json({
